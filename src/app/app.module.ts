69a49c641641deb740ecd676552787a4d6c6c4c2
web-dave <dave@webdave.de>
2017-12-27 10:27:01 +0100

    Generate a my-nav Component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 
 import { AppComponent } from './app.component';
+import { MyNavComponent } from './my-nav/my-nav.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MyNavComponent
   ],
   imports: [
     BrowserModule,
