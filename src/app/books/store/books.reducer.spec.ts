69074e2139a1011f95fffd5668ce7a76a524c6df
web-dave <dave@webdave.de>
2018-01-23 13:56:24 +0100

    reducer test

--- src/app/books/store/books.reducer.spec.ts
+++ src/app/books/store/books.reducer.spec.ts
@@ -1,19 +1,13 @@
-import { copyState, booksReducer } from "./books.reducer";
-import { bookMock, booksStub } from "../shared/books.service.stub";
+import { booksReducer, initialState } from "./books.reducer";
+import { booksStub } from "../shared/books.service.stub";
 import { LOAD_BOOKS } from "./books.actions";
 
 let oldstate = { books: booksStub }
-describe('copyState', () => {
-    it('should copy the state safely', () => {
-        let state = copyState(oldstate)
-        expect(oldstate === state).not.toBeTruthy()
-        expect(oldstate.books[0].title).toBe(state.books[0].title)
-    })
-})
+
 describe('booksReducer', () => {
     it('should load books', () => {
         let action = { type: LOAD_BOOKS, books: booksStub }
-        let state = booksReducer({ books: [] }, action)
+        let state = booksReducer(initialState, action)
         expect(state.books).toBe(booksStub)
     })
 })
