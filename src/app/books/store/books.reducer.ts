b9634fc30e6fce2e536fcc4cd312f5e45d49bf47
web-dave <dave@webdave.de>
2018-01-25 08:47:09 +0100

    reducer

--- src/app/books/store/books.reducer.ts
+++ src/app/books/store/books.reducer.ts
@@ -3,7 +3,7 @@ import { Action } from "@ngrx/store";
 import * as BooksActions from "./books.actions";
 import { IBook } from "../shared/custom-types";
 
-export const booksStoreName = 'books'
+export const booksStoreName = "books";
 
 export interface BooksState {
   books: IBook[];
@@ -13,17 +13,16 @@ export const initialState: BooksState = {
   books: []
 };
 
-export function copyState(state: BooksState): BooksState {
-  return JSON.parse(JSON.stringify(state));
-}
-
 export function booksReducer(state = initialState, action: Action): BooksState {
-  let newState = copyState(state);
   switch (action.type) {
     case BooksActions.LOAD_BOOKS:
-    const LOAD_BOOKS = <BooksActions.LoadBooks>action;
-    return Object.assign({}, state, {
-      books: LOAD_BOOKS.books
-    });
+      const LOAD_BOOKS = <BooksActions.LoadBooks>action;
+      return {
+        ...state,
+        books: LOAD_BOOKS.books
+      };
+
+    case BooksActions.READY_BOOKS:
+      return { ...state };
   }
 }
