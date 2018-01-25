a92e335f8d4c51260377ff48bcd3db0f4eb3825c
web-dave <dave@webdave.de>
2018-01-25 08:46:03 +0100

    ngrx tests

--- src/app/books/book-list/book-list.component.spec.ts
+++ src/app/books/book-list/book-list.component.spec.ts
@@ -1,34 +1,42 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
-import { DebugElement } from '@angular/core';
-import { By } from '@angular/platform-browser';
+import { async, ComponentFixture, TestBed } from "@angular/core/testing";
+import { DebugElement } from "@angular/core";
+import { By } from "@angular/platform-browser";
 
-import { BookListComponent } from './book-list.component';
-import { StoreModule, Store } from '@ngrx/store';
-import { BooksService } from '../shared/books.service';
-import { BooksState, booksStoreName } from '../store/books.reducer';
-import { booksStub, BooksServiceStub } from '../shared/books.service.stub';
-import { Observable } from 'rxjs';
-import { BookPreviewComponent } from '../shared/components.stubs';
-import { HttpClientModule } from '@angular/common/http';
-import { RouterTestingModule } from '@angular/router/testing';
+import { BookListComponent } from "./book-list.component";
+import { StoreModule, Store } from "@ngrx/store";
+import { BooksService } from "../shared/books.service";
+import { BooksState, booksStoreName } from "../store/books.reducer";
+import { booksStub, BooksServiceStub } from "../shared/books.service.stub";
+import { Observable } from "rxjs";
+import { BookPreviewComponent } from "../shared/components.stubs";
+import { HttpClientModule } from "@angular/common/http";
+import { RouterTestingModule } from "@angular/router/testing";
 
-describe('BookListComponent', () => {
+describe("BookListComponent", () => {
   let component: BookListComponent;
   let fixture: ComponentFixture<BookListComponent>;
   let store: Store<BooksState>;
   let elements: DebugElement[];
 
+  let state = {
+    books: booksStub
+  };
 
-  beforeEach(async(() => {
-    TestBed.configureTestingModule({
-      declarations: [BookListComponent, BookPreviewComponent],
-      imports: [StoreModule.forRoot({}), HttpClientModule, RouterTestingModule],
-      providers: [{ provide: BooksService, useClass: BooksServiceStub }]
+  beforeEach(
+    async(() => {
+      TestBed.configureTestingModule({
+        declarations: [BookListComponent, BookPreviewComponent],
+        imports: [
+          StoreModule.forRoot({}),
+          HttpClientModule,
+          RouterTestingModule
+        ],
+        providers: [{ provide: BooksService, useClass: BooksServiceStub }]
+      }).compileComponents();
+      store = TestBed.get(Store);
+      spyOn(store, "select").and.returnValue(Observable.of(booksStub));
     })
-      .compileComponents();
-    store = TestBed.get(Store);
-    spyOn(store, "select").and.returnValue(Observable.of([]))
-  }));
+  );
 
   beforeEach(() => {
     fixture = TestBed.createComponent(BookListComponent);
@@ -36,14 +44,13 @@ describe('BookListComponent', () => {
     fixture.detectChanges();
   });
 
-  it('should create', () => {
+  it("should create", () => {
     expect(component).toBeTruthy();
   });
 
-  it('should show 3 Books', () => {
-    component.books$ = Observable.of(booksStub)
+  it("should show 3 Books", () => {
     fixture.detectChanges();
-    elements = fixture.debugElement.queryAll(By.css('app-book-preview'));
+    elements = fixture.debugElement.queryAll(By.css("app-book-preview"));
     expect(elements.length).toBe(3);
   });
 });
--- src/app/books/store/books.reducer.spec.ts
+++ src/app/books/store/books.reducer.spec.ts
@@ -2,12 +2,19 @@ import { booksReducer, initialState } from "./books.reducer";
 import { booksStub } from "../shared/books.service.stub";
 import { LOAD_BOOKS } from "./books.actions";
 
-let oldstate = { books: booksStub }
+let oldstate = { books: booksStub };
 
-describe('booksReducer', () => {
-    it('should load books', () => {
-        let action = { type: LOAD_BOOKS, books: booksStub }
-        let state = booksReducer(initialState, action)
-        expect(state.books).toBe(booksStub)
-    })
-})
+describe("booksReducer", () => {
+  it("should load books", () => {
+    let action = { type: LOAD_BOOKS, books: booksStub };
+    let state = booksReducer(initialState, action);
+    expect(state.books).toBe(booksStub);
+  });
+  it("state should never be the same", () => {
+    let action = { type: LOAD_BOOKS, books: booksStub };
+    let state = booksReducer(initialState, action);
+    let newState = booksReducer(state, action);
+    expect(newState.books).toBe(state.books);
+    expect(newState === state).toBeFalsy();
+  });
+});
