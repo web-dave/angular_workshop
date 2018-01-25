745f7bc643e472d7b99b3d85a4ae02df566bad85
web-dave <dave@webdave.de>
2018-01-25 08:47:29 +0100

    reducer

--- src/app/books/store/books.reducer.ts
+++ src/app/books/store/books.reducer.ts
@@ -13,22 +13,16 @@ export const initialState: BooksState = {
   books: []
 };
 
-export function copyState(state: BooksState): BooksState {
-  return JSON.parse(JSON.stringify(state));
-}
-
 export function booksReducer(state = initialState, action: Action): BooksState {
   switch (action.type) {
     case BooksActions.LOAD_BOOKS:
       const LOAD_BOOKS = <BooksActions.LoadBooks>action;
-      console.log("Load Books!!!");
-      return Object.assign({}, state, {
+      return {
+        ...state,
         books: LOAD_BOOKS.books
-      });
+      };
 
     case BooksActions.READY_BOOKS:
-      const READY_BOOKS = <BooksActions.ReadyBooks>action;
-      console.log("Books Loaded!!!");
-      return Object.assign({}, state);
+      return { ...state };
   }
 }
