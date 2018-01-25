a41ebd1b851fa114d815b63288555a6febe4a246
web-dave <dave@webdave.de>
2018-01-25 08:46:31 +0100

    format

--- src/app/books/store/books.reducer.ts
+++ src/app/books/store/books.reducer.ts
@@ -6,23 +6,23 @@ import { IBook } from "../shared/custom-types";
 export const booksStoreName = "books";
 
 export interface BooksState {
-    books: IBook[];
+  books: IBook[];
 }
 
 export const initialState: BooksState = {
-    books: []
+  books: []
 };
 
 export function booksReducer(state = initialState, action: Action): BooksState {
-    switch (action.type) {
-        case BooksActions.LOAD_BOOKS:
-            const LOAD_BOOKS = <BooksActions.LoadBooks>action;
-            return {
-                ...state,
-                books: LOAD_BOOKS.books
-            };
+  switch (action.type) {
+    case BooksActions.LOAD_BOOKS:
+      const LOAD_BOOKS = <BooksActions.LoadBooks>action;
+      return {
+        ...state,
+        books: LOAD_BOOKS.books
+      };
 
-        case BooksActions.READY_BOOKS:
-            return { ...state };
-    }
+    case BooksActions.READY_BOOKS:
+      return { ...state };
+  }
 }
