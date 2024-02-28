489d68b9da138ef10fdff0f5102570c7cde91801
Dave <dave@webdave.de>
2024-02-28 11:15:20 +0100

    Child Routing

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,7 @@
 import { Routes } from '@angular/router';
 import { BooksComponent } from './books/books.component';
 import { AboutComponent } from './about/about.component';
+import { BookListComponent } from './book-list/book-list.component';
 
 export const routes: Routes = [
   {
@@ -11,6 +12,12 @@ export const routes: Routes = [
   {
     path: 'books',
     component: BooksComponent,
+    children: [
+      {
+        path: '',
+        component: BookListComponent,
+      },
+    ],
   },
   {
     path: 'about',
