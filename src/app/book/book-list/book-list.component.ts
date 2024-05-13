06784b8898cc128b91702ad878323fcf4c35d26c
Dave <dave@webdave.de>
2024-05-13 19:37:13 +0200

    @if @for

--- src/app/book/book-list/book-list.component.ts
+++ src/app/book/book-list/book-list.component.ts
@@ -1,12 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { BookService } from '../book.service';
 
 @Component({
   selector: 'app-book-list',
   standalone: true,
   imports: [],
   templateUrl: './book-list.component.html',
-  styleUrl: './book-list.component.scss'
+  styleUrl: './book-list.component.scss',
 })
 export class BookListComponent {
+  books: any;
+  foo = inject(BookService)
+    .getBooks()
+    .subscribe((data) => (this.books = data));
 
+  // service = inject(BookService);
+  // ngOnInit(): void {
+  //   this.service.getBooks().subscribe((data) => (this.books = data));
+  // }
 }
