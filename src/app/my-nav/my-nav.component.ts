1122393d0b3edb12d86ca7a2c38207cbbb29a92a
Dave <dave@webdave.de>
2024-05-13 13:42:07 +0200

    routing start

--- src/app/my-nav/my-nav.component.ts
+++ src/app/my-nav/my-nav.component.ts
@@ -1,12 +1,11 @@
 import { Component } from '@angular/core';
+import { RouterLink, RouterLinkActive } from '@angular/router';
 
 @Component({
   selector: 'app-my-nav',
   standalone: true,
-  imports: [],
+  imports: [RouterLink, RouterLinkActive],
   templateUrl: './my-nav.component.html',
-  styleUrl: './my-nav.component.scss'
+  styleUrl: './my-nav.component.scss',
 })
-export class MyNavComponent {
-
-}
+export class MyNavComponent {}
