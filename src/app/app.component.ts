5ca62e34a315e418215fa7f15b9c61660833e494
Dave <dave@webdave.de>
2025-03-10 16:38:06 +0100

    init v19

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,11 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
-import { MyNavComponent } from './my-nav/my-nav.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet, MyNavComponent],
+  imports: [CommonModule, RouterOutlet],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,18 +1,3 @@
 import { Routes } from '@angular/router';
-import { AboutComponent } from './about/about.component';
 
-export const routes: Routes = [
-  {
-    path: '',
-    redirectTo: '/books',
-    pathMatch: 'full',
-  },
-  {
-    path: 'books',
-    loadChildren: () => import('./books/books.routes'),
-  },
-  {
-    path: 'about',
-    component: AboutComponent,
-  },
-];
+export const routes: Routes = [];
