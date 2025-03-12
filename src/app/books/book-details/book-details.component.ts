df43d2a3e6636e9012fdf05be5f09e80ef1ce3f2
Dave <dave@webdave.de>
2025-03-12 16:09:12 +0100

    Async

--- src/app/books/book-details/book-details.component.ts
+++ src/app/books/book-details/book-details.component.ts
@@ -1,4 +1,5 @@
-import { Component, inject, OnInit } from '@angular/core';
+import { Component, DestroyRef, inject, OnInit } from '@angular/core';
+import { takeUntilDestroyed } from '@angular/core/rxjs-interop';
 import { IBook } from '../book';
 import { ActivatedRoute } from '@angular/router';
 import { BookService } from '../book.service';
@@ -8,16 +9,35 @@ import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-book-details',
-  imports: [],
+  imports: [PagesPipe, AsyncPipe],
   templateUrl: './book-details.component.html',
   styleUrl: './book-details.component.scss',
 })
 export class BookDetailsComponent implements OnInit {
-  book?: IBook;
+  _book?: IBook;
+  book$!: Observable<IBook>;
   route = inject(ActivatedRoute);
   service = inject(BookService);
+  dRef = inject(DestroyRef);
   ngOnInit(): void {
     const isbn = this.route.snapshot.params['isbn'];
-    this.service.getBook(isbn).subscribe((data) => (this.book = data));
+    this.book$ = this.service.getBook(isbn);
+    this.service
+      .getBook(isbn)
+      .pipe(takeUntilDestroyed(this.dRef))
+      .subscribe((data) => {
+        console.log('==>', data);
+        this._book = data;
+      });
+
+    // setInterval(() => {
+    //   if (this.book) {
+    //     this.book.numPages = 300;
+    //   }
+    // }, 1500);
   }
+  // transform(value: number, label = 'Pages'): string {
+  //   console.log('method', value);
+  //   return `${label}: ${value}`;
+  // }
 }
