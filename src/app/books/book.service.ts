f8bd45a3f306fb1c836aa5c7a2a5658872fd0bfe
Dave <dave@webdave.de>
2025-03-12 11:35:42 +0100

    use service

--- src/app/books/book.service.ts
+++ src/app/books/book.service.ts
@@ -1,13 +1,15 @@
 import { HttpClient } from '@angular/common/http';
 import { inject, Injectable } from '@angular/core';
+import { url } from '../../../env/env.dev';
 
 @Injectable({
   providedIn: 'root',
 })
 export class BookService {
+  url = url;
   service = inject(HttpClient);
 
   getBooks() {
-    return this.service.get('http://localhost:4730/books');
+    return this.service.get<any>('http://localhost:4730/books');
   }
 }
