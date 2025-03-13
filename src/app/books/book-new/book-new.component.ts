998bc8ca7fbd8557bbffe8bdf4c0bdc25e2e2192
Dave <dave@webdave.de>
2025-03-13 14:00:08 +0100

    Reactive forms möglichkeiten

--- src/app/books/book-new/book-new.component.ts
+++ src/app/books/book-new/book-new.component.ts
@@ -37,6 +37,30 @@ export class BookNewComponent implements OnInit {
     // this.newBookForm.get('author')
     // this.newBookForm.controls.author.disable();
     // console.log(this.newBookForm.getRawValue());
+
+    // this.newBookForm.valueChanges.subscribe((data) => console.log(data));
+    this.newBookForm.controls.author.valueChanges.subscribe((data) => {
+      if (data.toLowerCase() === 'peter') {
+        // this.newBookForm.controls.abstract.setValue('Paul & Marie');
+        // this.newBookForm.controls.abstract.disable();
+        this.newBookForm.controls.abstract.addValidators(
+          Validators.minLength(4)
+        );
+        // this.newBookForm.controls.abstract.removeValidators(
+        //   Validators.minLength(4)
+        // );
+        this.newBookForm.controls.abstract.updateValueAndValidity();
+
+        this.newBookForm.patchValue({
+          subtitle: 'Immer noch das Buch',
+          publisher: 'Mein Book Dealer',
+          price: '12,99',
+          numPages: 300,
+          cover: 'https://picsum.photos/200/300',
+          userId: 1,
+        });
+      }
+    });
   }
 
   saveBook() {
