bf05f6057ea8ff301b2ae847d2e7be50b8ddd23e
Dave <dave@webdave.de>
2025-03-13 14:20:16 +0100

    Form validator

--- src/app/books/book-new/book-new.component.ts
+++ src/app/books/book-new/book-new.component.ts
@@ -1,12 +1,39 @@
 import { Component, inject, OnInit } from '@angular/core';
 import {
+  AbstractControl,
   NonNullableFormBuilder,
   ReactiveFormsModule,
+  ValidationErrors,
+  ValidatorFn,
   Validators,
 } from '@angular/forms';
 import { IBook } from '../book';
 import { BookService } from '../book.service';
 import { ActivatedRoute, Router } from '@angular/router';
+
+const authorValidator: ValidatorFn = (
+  control: AbstractControl
+): ValidationErrors | null => {
+  const authors = ['peter', 'paul', 'marie'];
+  const result = authors.includes(control.value.toLowerCase());
+  return result
+    ? null
+    : {
+        author: 'Das ist kein registrierter Author.',
+      };
+};
+
+const authorValidatorFactory =
+  (authors: string[]) =>
+  (control: AbstractControl): ValidationErrors | null => {
+    // const authors = ['peter', 'paul', 'marie'];
+    const result = authors.includes(control.value.toLowerCase());
+    return result
+      ? null
+      : {
+          author: 'Das ist kein registrierter Author.',
+        };
+  };
 
 @Component({
   selector: 'app-book-new',
@@ -15,13 +42,14 @@ import { ActivatedRoute, Router } from '@angular/router';
   styleUrl: './book-new.component.scss',
 })
 export class BookNewComponent implements OnInit {
+  authors = ['peter', 'paul', 'marie'];
   newBookForm = inject(NonNullableFormBuilder).group({
     id: ['', []],
     title: ['', [Validators.required]],
     subtitle: ['', []],
     isbn: ['', [Validators.required, Validators.minLength(5)]],
     abstract: ['', [Validators.required]],
-    author: ['', [Validators.required]],
+    author: ['', [Validators.required, authorValidatorFactory(this.authors)]],
     publisher: ['', []],
     price: ['', []],
     numPages: [0, []],
